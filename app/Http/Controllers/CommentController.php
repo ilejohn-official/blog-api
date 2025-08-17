92ad128a96c59fe8af96c4a42c2d41f7a5b1a385
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-08-17 16:50:02 +0100

    scaffold controller methods for blog API

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+class CommentController extends Controller
+{
+    /**
+     * Display the specified resource.
+     */
+    public function like(Post $post)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class LikeController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function comment(Post $post)
+    {
+        //
+    }
+}
