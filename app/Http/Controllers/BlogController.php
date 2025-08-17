1c6713be946e989ced7a109127896413b83577da
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-08-17 17:26:40 +0100

    implement operations for blog crud

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,47 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Requests\StoreBlogRequest;
+use App\Http\Requests\UpdateBlogRequest;
 
 class BlogController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        return response()->json(Blog::paginate(10));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreBlogRequest $request): JsonResponse
     {
-        //
+        $blog = Blog::create($request->validated());
+
+        return response()->json($blog, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Blog $blog)
+    public function show(Blog $blog): JsonResponse
     {
-        //
+        return response()->json($blog->load('posts'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Blog $blog)
+    public function update(UpdateBlogRequest $request, Blog $blog): JsonResponse
     {
-        //
+        $blog->update($request->validated());
+
+        return response()->json($blog);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Blog $blog)
+    public function destroy(Blog $blog): JsonResponse
     {
-        //
+        $blog->delete();
+
+        return response()->json(['message' => 'Blog deleted successfully']);
     }
 }
